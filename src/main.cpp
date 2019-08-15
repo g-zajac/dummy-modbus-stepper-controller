b9b205b99ee04b827ba2bde236370ce280136a38
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-08-15 16:05:24 +0200

    add nonblocking case in loop for monitoring

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,8 @@ byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };
 EthernetClient client;
 int  interval = 1000; // Wait between dumps
 
-void setup() {
-
-  Serial.begin(9600);
-  Ethernet.begin(mac);
-
+void printIPAddress()
+{
   Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
   Serial.print("IP Address        : ");
   Serial.println(Ethernet.localIP());
@@ -25,17 +22,55 @@ void setup() {
   Serial.println(Ethernet.gatewayIP());
   Serial.print("DNS Server IP     : ");
   Serial.println(Ethernet.dnsServerIP());
+  Serial.println();
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+      Serial.println("Failed to configure Ethernet using DHCP");
+      // no point in carrying on, so do nothing forevermore:
+      for (;;);
+    }
+    // print your local IP address:
+    printIPAddress();
 }
 
 void loop() {
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("-> Connected");
-  }
-  else {
-    // you didn't get a connection to the server:
-    Serial.println("--> connection failed/n");
-  }
+    switch (Ethernet.maintain())
+     {
+       case 1:
+         //renewed fail
+         Serial.println("Error: renewed fail");
+         break;
 
-  delay(interval);
+       case 2:
+         //renewed success
+         Serial.println("Renewed success");
+
+         //print your local IP address:
+         printIPAddress();
+         break;
+
+       case 3:
+         //rebind fail
+         Serial.println("Error: rebind fail");
+         break;
+
+       case 4:
+         //rebind success
+         Serial.println("Rebind success");
+
+         //print your local IP address:
+         printIPAddress();
+         break;
+
+       default:
+         //nothing happened
+         break;
+
+     }
 }
