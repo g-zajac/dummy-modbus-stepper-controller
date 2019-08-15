d9ec82a9667982ff798fcb22831f33cfd56883c2
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-08-15 15:54:07 +0200

    add dhcp client test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+#include <UIPEthernet.h> // Used for Ethernet
+
+// **** ETHERNET SETTING ****
+// Arduino Uno pins: 10 = CS, 11 = MOSI, 12 = MISO, 13 = SCK
+// Ethernet MAC address - must be unique on your network - MAC Reads T4A001 in hex (unique in your network)
+byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };
+// For the rest we use DHCP (IP address and such)
+
+EthernetClient client;
+int  interval = 1000; // Wait between dumps
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+
+  Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+  Serial.print("IP Address        : ");
+  Serial.println(Ethernet.localIP());
+  Serial.print("Subnet Mask       : ");
+  Serial.println(Ethernet.subnetMask());
+  Serial.print("Default Gateway IP: ");
+  Serial.println(Ethernet.gatewayIP());
+  Serial.print("DNS Server IP     : ");
+  Serial.println(Ethernet.dnsServerIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("-> Connected");
+  }
+  else {
+    // you didn't get a connection to the server:
+    Serial.println("--> connection failed/n");
+  }
+
+  delay(interval);
 }
