d5cdf0ec2b71d332d6d4e60be69fa6b89b99a142
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-09-01 21:50:15 +0200

    remove interval redundant variable

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 byte mac[] = { 0x54, 0x34, 0x41, 0x30, 0x30, 0x31 };
 // For the rest we use DHCP (IP address and such)
 
-int  interval = 1000; // Wait between dumps
-
 void printIPAddress()
 {
   Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
@@ -27,13 +25,13 @@ void printIPAddress()
 void setup() {
 
   Serial.begin(9600);
-
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
       Serial.println("Failed to configure Ethernet using DHCP");
       // no point in carrying on, so do nothing forevermore:
       for (;;);
     }
+
     // print your local IP address:
     printIPAddress();
 }
