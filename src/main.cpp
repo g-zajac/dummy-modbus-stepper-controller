993f0de117fe99e2ecb94287274900f11178063a
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-10-15 15:39:28 +0200

    working example, ETH_CLK, PHY defined fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,16 @@
 /*
-    This sketch shows how to configure different external or internal clock sources for the Ethernet PHY
+    This sketch shows the Ethernet event usage
+
 */
+#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+#define ETH_PHY_POWER 12
 
 #include <ETH.h>
 
-/*
-   * ETH_CLOCK_GPIO0_IN   - default: external clock from crystal oscillator
-   * ETH_CLOCK_GPIO0_OUT  - 50MHz clock from internal APLL output on GPIO0 - possibly an inverter is needed for LAN8720
-   * ETH_CLOCK_GPIO16_OUT - 50MHz clock from internal APLL output on GPIO16 - possibly an inverter is needed for LAN8720
-   * ETH_CLOCK_GPIO17_OUT - 50MHz clock from internal APLL inverted output on GPIO17 - tested with LAN8720
-*/
-#ifdef ETH_CLK_MODE
-#undef ETH_CLK_MODE
-#endif
-#define ETH_CLK_MODE    ETH_CLOCK_GPIO17_OUT
-// #define ETH_CLK_MODE    ETH_CLOCK_GPIO0_IN
-
-// Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define ETH_POWER_PIN   -1
-
-// Type of the Ethernet PHY (LAN8720 or TLK110)
-#define ETH_TYPE        ETH_PHY_LAN8720
-
-// I²C-address of Ethernet PHY (0 or 1 for LAN8720, 31 for TLK110)
-#define ETH_ADDR        0
-
-// Pin# of the I²C clock signal for the Ethernet PHY
-#define ETH_MDC_PIN     15
-
-// Pin# of the I²C IO signal for the Ethernet PHY
-#define ETH_MDIO_PIN    2
-
-
 static bool eth_connected = false;
 
-void WiFiEvent(WiFiEvent_t event) {
+void WiFiEvent(WiFiEvent_t event)
+{
   switch (event) {
     case SYSTEM_EVENT_ETH_START:
       Serial.println("ETH Started");
@@ -70,7 +46,8 @@ void WiFiEvent(WiFiEvent_t event) {
   }
 }
 
-void testClient(const char * host, uint16_t port) {
+void testClient(const char * host, uint16_t port)
+{
   Serial.print("\nconnecting to ");
   Serial.println(host);
 
@@ -89,23 +66,18 @@ void testClient(const char * host, uint16_t port) {
   client.stop();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.onEvent(WiFiEvent);
-  ETH.begin(ETH_ADDR, ETH_POWER_PIN, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_TYPE, ETH_CLK_MODE);
+  ETH.begin();
 }
 
 
-void loop() {
+void loop()
+{
   if (eth_connected) {
     testClient("google.com", 80);
   }
   delay(10000);
-  Serial.print("eth state: ");
-  Serial.println(eth_connected);
-  Serial.print("ETH MAC: ");
-  Serial.print(ETH.macAddress());
-  Serial.print(", IPv4: ");
-  Serial.print(ETH.localIP());
-
 }
