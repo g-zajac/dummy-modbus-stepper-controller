15cf9f8ab3154f6fb30e0889c331192ba966da20
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-10-25 00:38:22 +0200

    add soldering description and grafics draft

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,19 @@
 // https://github.com/andresarmento/modbus-arduino
 #include <Modbus.h>
 #include <ModbusIP.h>
-
+bool modbusConnected = false;
 // adafruit OLED pinmap: data - sda (pin18), clk - scl (pin19), reset pin 20
+// DATA - white (no1 from Teensy side) - pin 18
+// CLK - yellow 2 -pin 19
+// GND - black
+// 5V - red
 // TODO test if reset is needed for oled?
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 int progres_counter = 0;
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     20 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -47,6 +52,18 @@ int position = 0;
 EthernetServer server = EthernetServer(502);
 
 // **** ETHERNET SETTING ****
+/*
+RED (1) - PoE-
+(2) - PoE +
+(3) - GND
+(4) - MI - MISO - pin 12
+(5) - MO - MOSI - pin 11
+(6) - SCK - pin 14
+(7) - NSS - CS pin 10
+(8) - RST
+(9) - 5V+
+(10) - GND
+*/
 // Arduino Uno pins: 10 = CS, 11 = MOSI, 12 = MISO, 13 = SCK
 // Teens 10 = CS(SS), 11 = MOSI, 12 = MISO, 14 (changed from 13 in code) = SCK
 // Ethernet MAC address - must be unique on your network - MAC Reads T4A001 in hex (unique in your network)
@@ -155,6 +172,28 @@ void loop() {
      mb.Hreg(HREG_ALARM_CODE, alarmState);
     }
 
+    // check if modbus client is connected:
+    // if (EthernetServer.connected()){
+    //   modbusConnected = true;
+    // }
+    // else {
+    //   modbusConnected = false;
+    // };
+
+    Serial.println(server.available());
+    Serial.println(Ethernet.hardwareStatus());
+
+    if (Ethernet.linkStatus() == Unknown) {
+      Serial.println("Link status unknown. Link status detection is only available with W5200 and W5500.");
+      }
+      else if (Ethernet.linkStatus() == LinkON) {
+        Serial.println("Link status: On");
+      }
+      else if (Ethernet.linkStatus() == LinkOFF) {
+        Serial.println("Link status: Off");
+    }
+
+
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
@@ -180,20 +219,29 @@ void loop() {
 
 
       // draw cicrcle at initialisatio
-      // - \ | /
+      // - "\\" \ | /
+      // TO DO TEST fonts: ◴ ◷ ◶ ◵
+      // https://raw.githubusercontent.com/sindresorhus/cli-spinners/master/spinners.json
+      // more frames ? al'a js?
+
+      // if modbus connected
+      // if (mb.connected())
+      Serial.print("modbus connected: ");
+      Serial.println(modbusConnected);
+
       if (progres_counter <5) {
         display.setCursor(12,0);
         display.print(" ");
         display.setCursor(12,0);
         switch(progres_counter){
           case 0:
-          display.print("|"); break;
+          display.print("◴"); break;
           case 1:
-          display.print("/"); break;
+          display.print("◷"); break;
           case 2:
-          display.print("-"); break;
+          display.print("◶"); break;
           case 3:
-          display.print('\\'); break;
+          display.print("◵"); break;
         }
         display.display();
         progres_counter++;
