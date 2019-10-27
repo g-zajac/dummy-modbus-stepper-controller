67205400ce949fcdc482b393dd6b0259d266fe7c
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-10-27 16:20:52 +0100

    remove serial print mac addr

--- src/main.cpp
+++ src/main.cpp
@@ -210,7 +210,6 @@ void loop() {
 
       char buf_mac[18];
       sprintf(buf_mac, "mac:%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-      Serial.print("MAC: "); Serial.println(buf_mac);
       displayOnOled(buf_mac, 2);
 
       // update IP Address
