8eade07964f2347c69b8d56809450c9e4cc49a36
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-11-01 09:23:28 +0100

    add motor current measurment

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define FIRMWARE_VERSION 1.22
+#define FIRMWARE_VERSION 1.25
 #include <Arduino.h>
 
 // #include <SPI.h>
@@ -50,7 +50,13 @@ const int alarmLedPin = 16;
 const int knobButtonPin = 15;
 bool alarmState = 0;
 
-const int currentAD = A9; //gpio 23
+// Measuring Current Using ACS712
+const int analogINpin = A9;
+const float offset = 2.4;
+float calibration = -0.100;
+int noSamples = 100; //numbers of AD samples taken to count average
+float voltage = 0;
+float current = 0;
 
 #include <AccelStepper.h>
 
@@ -72,7 +78,6 @@ const int HREG_COMMAND_OPCODE = 40125;
 //ModbusIP object
 ModbusIP mb;
 long ts;
-int analogIn = A6; // GPIO4
 
 // Set Port to 502
 EthernetServer server = EthernetServer(502);
@@ -199,6 +204,9 @@ void setup() {
   mb.addHreg(HREG_COMMAND_OPCODE, 0);
 
   // set stepper motor
+
+  //disable motor temporary
+
   pinMode(enable, OUTPUT);
   digitalWrite(enable, LOW);
   stepper.setMaxSpeed(1000.0);
@@ -231,6 +239,8 @@ void loop() {
       mb.Hreg(HREG_IMEDIATE_ABSOLUTE_POSITION, knob_position);
     };
 
+
+
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
@@ -259,6 +269,22 @@ void loop() {
       sprintf(buf_reg2, "pos: %d", mb.Hreg(HREG_IMEDIATE_ABSOLUTE_POSITION));
       displayOnOled(buf_reg2, 5);
 
+      // Measuring Current Using ACS712
+      // average AD reading
+      float samplesVal = 0;
+      float ascValue = 0;
+      for (int i =0; i < noSamples; i++){
+        ascValue = analogRead(analogINpin);
+        samplesVal = samplesVal + ascValue;
+        delay(3);
+      }
+      float voltage_avg = samplesVal / noSamples;
+      voltage = ((3.3 / 1023.0) * voltage_avg) - 2.4 + calibration; // (max AD voltage / resolution) - offset
+      current = voltage / 0.185;  // sesnsitivity 185mV
+      char buf_I[18];
+      sprintf(buf_I, "stepI: %.3f", current);
+      displayOnOled(buf_I, 6);
+
       if (progres_counter < 4) {
         display.setCursor(6*20,0);  //max 20 characters in line
         display.print(" ");
@@ -271,12 +297,11 @@ void loop() {
           case 2:
           display.print("|"); break;
           case 3:
-          display.print("/"); break;
+          display.print("/");break;
         }
         display.display();
         progres_counter++;
       } else progres_counter = 0;
-
     } // end of interval
 
     switch (Ethernet.maintain())
